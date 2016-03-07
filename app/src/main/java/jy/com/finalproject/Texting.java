ed7bcf056ff42d41c7c811cb9e397c4c6b3161b3
fernietapia <fctapia@ucsc.edu>
2016-03-06 21:44:34 -0800

    Locked screen
    Screen wont rotate now

--- app/src/main/java/jy/com/finalproject/Texting.java
+++ app/src/main/java/jy/com/finalproject/Texting.java
@@ -1,15 +1,6 @@
 package jy.com.finalproject;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
 import android.telephony.SmsManager;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 /**
  * Created by fernie on 3/6/2016.
@@ -19,17 +10,7 @@ public class Texting {
     //String sms = messageBody.getText().toString();
 
     public void sendText() {
-        try {
             SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(number, null, sms, null, null);
-            Toast.makeText(getApplicationContext(), "SMS Sent!",
-                    Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
-            Toast.makeText(getApplicationContext(),
-                    "SMS faild, please try again later!",
-                    Toast.LENGTH_LONG).show();
-            e.printStackTrace();
-        }
-
+            //smsManager.sendTextMessage(number, null, sms, null, null);
     }
 }
